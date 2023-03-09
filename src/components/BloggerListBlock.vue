894a00381b2b43dbe2f0ff95bb391f07235a09af
Aliskhan <aliskhan2021@mail.ru>
2023-03-09 21:38:36 +0300

    blogger-list-component done

--- src/components/BloggerListBlock.vue
+++ src/components/BloggerListBlock.vue
@@ -3,15 +3,27 @@
   <section class="blogger-list">
     <div class="container">
       <div class="blogger-list__wrapper">
-				
+        <div class="blogger-list__topbar">
+          <p class="blogger-list__found">
+            Найдено:
+            <span class="blogger-list__found-number">119</span>
+          </p>
+          <a-button class="blogger-list__upload-button">
+            <template #icon>
+              <DownloadOutlined />
+            </template>
+            Выгрузить 20 результатов
+          </a-button>
+          <a-select class="blogger-list__sort"></a-select>
+        </div>
       </div>
     </div>
   </section>
 </template>
 <script>
-// import { def } from "@vue/shared";
 // import FiltersTagsBlock from "./FiltersTagsBlock.vue";
-// export default {
-//   components: { FiltersTagsBlock },
-// };
-// </script>
+import { DownloadOutlined } from "@ant-design/icons-vue";
+export default {
+  components: { DownloadOutlined },
+};
+</script>
