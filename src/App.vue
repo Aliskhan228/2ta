a5148ebcf8f41cffff98358852cc009d78c0b9f0
Nikita Kutkovoy <nikita.kutkovoy@gmail.com>
2023-03-10 02:18:05 +0200

    Basic list structure and card implemented

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <section class="app">
     <HeaderBlock />
-		<FiltersBlock />
+    <FiltersBlock />
+    <BloggerListBlock />
   </section>
 </template>
 <script>
 import HeaderBlock from './components/HeaderBlock.vue';
 import FiltersBlock from './components/FiltersBlock.vue';
+import BloggerListBlock from './components/BloggerListBlock.vue';
 export default {
-  components: { HeaderBlock, FiltersBlock }
+  components: { HeaderBlock, FiltersBlock, BloggerListBlock }
 }
 </script>
--- src/components/BloggerListBlock.vue
+++ src/components/BloggerListBlock.vue
@@ -1,3 +1,67 @@
 <template>
-  <!-- Write HTML here -->
+  <a-list class="demo-loadmore-list" :loading="initLoading" item-layout="horizontal" :data-source="list">
+    <template #loadMore>
+      <div v-if="!initLoading && !loading"
+        :style="{ textAlign: 'center', marginTop: '12px', height: '32px', lineHeight: '32px' }">
+        <a-button @click="onLoadMore">Показать еще</a-button>
+      </div>
+    </template>
+    <template #renderItem="{ item }">
+      <a-skeleton :loading="!!item.loading" active>
+        <YouTubeCard :avatarSrc="item.picture.large" />
+      </a-skeleton>
+    </template>
+  </a-list>
 </template>
+<script>
+import { defineComponent, onMounted, ref, nextTick } from 'vue';
+import YouTubeCard from './YouTubeCard.vue';
+const count = 1;
+const fakeDataUrl = `https://randomuser.me/api/?results=${count}&inc=name,gender,email,nat,picture&noinfo`;
+export default defineComponent({
+  components: { YouTubeCard },
+  setup() {
+    const initLoading = ref(true);
+    const loading = ref(false);
+    const data = ref([]);
+    const list = ref([]);
+    onMounted(() => {
+      fetch(fakeDataUrl).then(res => res.json()).then(res => {
+        initLoading.value = false;
+        data.value = res.results;
+        list.value = res.results;
+      });
+    });
+    const onLoadMore = () => {
+      loading.value = true;
+      list.value = data.value.concat([...new Array(count)].map(() => ({
+        loading: true,
+        name: {},
+        picture: {},
+      })));
+      fetch(fakeDataUrl).then(res => res.json()).then(res => {
+        const newData = data.value.concat(res.results);
+        loading.value = false;
+        data.value = newData;
+        list.value = newData;
+        nextTick(() => {
+          // Smooth scroll to bottom
+          window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth" });
+        });
+      });
+    };
+    return {
+      loading,
+      initLoading,
+      data,
+      list,
+      onLoadMore,
+    };
+  },
+});
+</script>
+<style scoped>
+.demo-loadmore-list {
+  min-height: 350px;
+}
+</style>
